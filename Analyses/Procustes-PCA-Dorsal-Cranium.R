d58647c8191a48ce7e2eb4dfa0205a8e0742ccd5
christiancjw <c.ching@nhm.ac.uk>
2021-04-05 15:09:01 +0100

    Added additional comments to script

--- Analyses/Procustes-PCA-Dorsal-Cranium.R
+++ Analyses/Procustes-PCA-Dorsal-Cranium.R
@@ -1,12 +1,19 @@
-#Load tidyverse (Use of data viewing functions), geomorph (For landmark analyses),
-## ggplot2 (For creating figures)
+# SETUP ---- 
+## Geometric morphometric analyses of Galeopterus Skulls 
+## Set working directory to folder for project, or open project at new directory. 
+## Version control repository established with GitHub
+
+# Load libraries:
+## tidyverse (Use of data viewing functions), geomorph (For landmark analyses),
+## ggplot2 (For creating figures), dplyr (for data manipulation)
 library(geomorph)
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
 
-# All images have been scaled in imageJ.
-## Load files into R
+# Inputting of coordinate data ----
+# TPS file created from scaled landmarks measured in ImageJ
+# Read in landmark coordinates
 lands <- readland.tps(file = "Rawdata/dorsal.crania.tps", specID = "ID")
 lands
 
@@ -14,6 +21,7 @@ lands
 ## to rotation and scaling)
 plot(lands[,2,]~lands[,1,])
 
+# Normalisation of data through Generalised Procustes Analysis ----
 ## Procrustes analyses - uses reference specimen to line up all specimens 
 gpa.lands <- gpagen(lands)
 
@@ -27,11 +35,12 @@ gpa.lands$coords
 # Expanded information of gpa.lands. (Str:structure)
 str(gpa.lands)
 
-# Join metadata to tps file
+
+# Reading in of metadata ----
 metadata <- read.csv("Rawdata/dermopteradata.csv")
 glimpse(metadata)
 
-## PCA Analysis
+## PCA Analysis ----
 pca.landmarks <- gm.prcomp(gpa.lands$coords)
 pca.landmarks
 
@@ -39,7 +48,7 @@ pca.landmarks
 summary(pca.landmarks)
 ## Can be seen that 16 principal components explains for 95.7% of variation
 
-# Merge PC scores and metadata
+# Merge PC scores and metadata ----
 # Extract PC scores and make ID name from TPS into a taxon column for 
 ## combining with the metadata
 pc_scores <- data.frame(specimenID = rownames(pca.landmarks$x), 
@@ -49,10 +58,10 @@ pc_scores
 # Make column names begin with PC not Comp
 colnames(pc_scores) <- gsub("Comp", "PC", colnames(pc_scores))
 
-# Merge with metadata
+# Merge with metadata ----
 pc_data <- full_join(pc_scores, metadata, by = c("specimenID" = "DorsalID"))
 
-# Making a new dataset of just galeopterus (excluding volans)
+# Making a new dataset of just galeopterus (excluding volans) ----
 # The ! is used to reverse arguements
 g_data <- pc_data
 gv_data <- filter(g_data, !Region == "Philippines")
@@ -61,14 +70,14 @@ gv_data <- filter(g_data, !Region == "Philippines")
 ggplot(gv_data, aes(x=PC1, y=PC2, colour=Region)) + 
   geom_point()
 
-# Write to file
+# Write to file ----
 write_csv(pc_data, path = "~/Users/christianching/Desktop/
           Galeopterus/RProject/galeopterus.skulls/Data/colugo-pca-data-dorsal.csv") 
 
 ##??
 pca.landmarks$rotation
 
-# Plotting PC Data. 
+# Plotting PC Data. ----
 ## Aesthetics(X var, Y Var, shape based variable, colour based variable)
 ## geom_label(aes(label=XX))
 ggplot(pc_data, aes(x=PC1, y=PC2, shape=CurrentSp, colour=Region)) + 
