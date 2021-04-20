9d93e8b63cbe38232af63ec63c768d7362a14ae2
christiancjw <c.ching@nhm.ac.uk>
2021-04-20 23:23:31 +0100

    Added some MANOVA lines

--- Analyses/MANOVA.R
+++ Analyses/MANOVA.R
@@ -7,17 +7,42 @@ pc_data <- read_csv(file = "Rawdata/colugo-pca-data-dorsal.csv")
 # Multivariate analysis of variance for Dermoptera Data ----
 
 # MANOVA of Dermoptera PCA data by species (Philippine vs Sunda)
-derm.manova <- manova(cbind(pc_data$PC1, pc_data$PC2) ~ CurrentSp,
+derm.sp.manova <- manova(cbind(pc_data$PC1, pc_data$PC2) ~ CurrentSp,
                    data = pc_data)
-anova(derm.manova)
+summary(derm.sp.manova)
+
+# MANOVA of Dermopetra PCA data by region
+derm.r.manova <- manova(cbind(pc_data$PC1, pc_data$PC2) ~ Region,
+                      data = pc_data)
+summary(derm.r.manova)
+
+# MANOVA of Dermoptera PCA data by sex
+derm.s.manova <- manova(cbind(pc_data$PC1, pc_data$PC2) ~ Sex,
+                        data = pc_data)
+summary(derm.s.manova)
+
+# MANOVA of Dermopetra PCA data by date photographed
+derm.d.manova <- manova(cbind(pc_data$PC1, pc_data$PC2) ~ DatePhotographed,
+                        data = pc_data)
+summary(derm.d.manova)
 
 # Multivariate analysis of variance for Sunda Data ----
 
 # MANOVA of G.variegatus PCA data by region
 sunda.manova <- manova(cbind(sunda_pc_data$PC1, sunda_pc_data$PC2) ~ Region,
                        data = sunda_pc_data)
-
+summary(sunda.manova)
 anova(sunda.manova)
+
+# MANOVA of G.variegatus PCA data by sex
+sunda.s.manova <- manova(cbind(pc_data$PC1, pc_data$PC2) ~ Sex,
+                        data = sunda_pc_data)
+summary(derm.s.manova)
+
+# MANOVA of G.variegatus PCA data by date photographed
+sunda.d.manova <- manova(cbind(pc_data$PC1, pc_data$PC2) ~ DatePhotographed,
+                        data = pc_data)
+summary(derm.d.manova)
 
 # Misc Notes ----
 
